7046e89debb0057b62feca347cfe1a79e1252351
Matthew Yong <yongf@student.unimelb.edu.au>
2018-11-12 16:32:10 +1100

    - Frame Rendering Feature Added

--- Pong_Game_Solution/Pong_Game_Solution/main.cpp
+++ Pong_Game_Solution/Pong_Game_Solution/main.cpp
@@ -6,15 +6,35 @@ Game *game = nullptr;
 
 int main(int argc, const char** argv) {
 
+	//FPS Handling
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 framestart;
+	int frameTime;
+
 	game = new Game();
 
 	game->init("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
 	//Run the game
 	while (game->running()) {
+
+		//How many ms since we first initialize SDL
+		framestart = SDL_GetTicks();
+
 		game->handleEvents();
 		game->update();
 		game->render();
+
+		//Time in ms in handling, updating and rendering
+		frameTime = SDL_GetTicks() - framestart;
+
+		//We need to delay in this case
+		if (frameDelay > frameTime) {
+			SDL_Delay(frameDelay - frameTime);
+		}
+
 	}
 
 	//Game is exited and objects cleaned
